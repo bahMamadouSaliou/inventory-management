498b54af30494b598aba48e44a81163e49e84b4f
BAH Mamadou Saliou <49727147+bahMamadouSaliou@users.noreply.github.com>
2024-12-03 02:18:41 +0100

    initial commit

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import DashboardWrapper from "./dashboardWrapper";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "ABB SARLU",
+  description: "Inventory management system",
+  icons: {
+    icon: "/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.svg" />
+      </head>
+      <body className={inter.className}>
+        <DashboardWrapper>{children}</DashboardWrapper>
+      </body>
+    </html>
+  );
+}
